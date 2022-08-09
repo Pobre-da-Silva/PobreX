2ee068b111707e7797f8b3baa1985fc268e65262
jonasbrocas <55618670+jonasbrocas007@users.noreply.github.com>
2022-08-09 15:57:09 +0100

    Add files via upload

--- PobreX.sh
+++ PobreX.sh
@@ -4,21 +4,25 @@ echo "installing..."
 #dependencies
 sudo pacman -Syu
 sudo pacman -S nodejs
+sudo pacman -S npm
 sudo pacman -S python3
 sudo pacman -S xorg
 sudo pacman -S git
-npm install x11
+npm i x11
 sudo pacman -S xterm
+sudo pacman -S xorg-xinit
 #programs
-npm install slugdown
+npm i slugdown
 npm i dwing
 git clone https://github.com/Pobre-da-Silva/pobrelang
 git clone https://github.com/Pobre-da-Silva/prume
+
 #nano is for pussys, real boys use prume
 sudo pacman -R nano
 
+#firefox
 sudo pacman -S firefox
-sudo pacman -S picom
+
 
 
 
